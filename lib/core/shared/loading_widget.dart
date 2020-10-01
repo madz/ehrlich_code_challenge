65ce1630330afcba55bb050ac621f3488cf7ffaf
madz <markdiesta@gmail.com>
2020-10-02 07:47:47 +0800

    Update loading_widget.dart
    Added barrierDismissibl: false in showing material dialog.

--- lib/core/shared/loading_widget.dart
+++ lib/core/shared/loading_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class LoadingWidget {
   showMaterialDialog(BuildContext context) {
     showDialog(
+      barrierDismissible: false,
       context: context,
       builder: (_) => new AlertDialog(
         title: LinearProgressIndicator(),
