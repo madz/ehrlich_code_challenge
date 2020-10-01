099f85c22dbbc6a70df062f4e92ab5bac912b55f
madz <markdiesta@gmail.com>
2020-10-02 01:33:03 +0800

    Create loading_widget.dart

---
+++ lib/core/shared/loading_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadingWidget {
+  showMaterialDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+        title: LinearProgressIndicator(),
+      ),
+    );
+  }
+
+  showCupertinoDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) => CupertinoAlertDialog(
+        content: CupertinoActivityIndicator(),
+      ),
+    );
+  }
+}
