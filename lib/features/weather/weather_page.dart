3c9a795c504b807f65f7776bae850e6c2785acac
madz <markdiesta@gmail.com>
2020-10-02 11:09:59 +0800

    Update weather_page.dart
    Update format of grid text and allignment.

--- lib/features/weather/weather_page.dart
+++ lib/features/weather/weather_page.dart
@@ -3,6 +3,7 @@ import 'package:ehrlich_code_challenge/core/models/weather_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_device_type/flutter_device_type.dart';
+import 'package:intl/intl.dart';
 
 class WeatherPage extends StatelessWidget {
   @override
@@ -15,64 +16,178 @@ class WeatherPage extends StatelessWidget {
       body: BlocBuilder<WeatherCubit, WeatherState>(
         builder: (_, weatherState) {
           final WeatherModel weatherModel = weatherState.weatherModel;
+          final DateTime date = DateTime.now();
+
+          var formatDate = DateFormat.yMd().format(date);
           if (weatherState.isSuccess) {
             if (Device.get().isTablet) {
               return GridView.count(
-                crossAxisCount: 5,
+                crossAxisCount: 6,
                 children: [
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text('City'),
-                      Text('${weatherModel.name}'),
+                      Card(
+                        child: Text(
+                          'Date \n (mm/dd/yyyy)',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '$formatDate',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text('Country'),
-                      Text('${weatherModel.sys.country}'),
+                      Card(
+                        child: Text(
+                          'Temp(F)',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '${weatherModel.main.temp}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text('Weather'),
-                      Text('${weatherModel.weather[0].main}'),
+                      Card(
+                        child: Text(
+                          'Description',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '${weatherModel.weather[0].description}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text('Temperature'),
-                      Text('(F)'),
-                      Text('${weatherModel.main.temp}'),
+                      Card(
+                        child: Text(
+                          'Main',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '${weatherModel.weather[0].main}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text('Wind speed'),
-                      Text('${weatherModel.wind.speed}'),
+                      Card(
+                        child: Text(
+                          'Pressure',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '${weatherModel.main.pressure}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Card(
+                        child: Text(
+                          'Humidity',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Card(
+                        child: Text(
+                          '${weatherModel.main.humidity}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                 ],
               );
             }
             return GridView.count(
-              crossAxisCount: 3,
+              crossAxisCount: 2,
               children: [
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text('City'),
-                    Text('${weatherModel.name}'),
+                    Card(
+                      child: Text(
+                        'Date(mm/dd/yyyy)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Card(
+                      child: Text(
+                        '$formatDate',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ],
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text('Country'),
-                    Text('${weatherModel.sys.country}'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text('Weather'),
-                    Text('${weatherModel.weather[0].main}'),
+                    Card(
+                      child: Text(
+                        'Temperature(F)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Card(
+                      child: Text(
+                        '${weatherModel.main.temp}',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ],
                 ),
               ],
